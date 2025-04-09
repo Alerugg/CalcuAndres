2d14fd2f94c20e7f3218f4b75fb9da03647bd6e0
Alejandro Ruggeri <134484194+Alerugg@users.noreply.github.com>
2025-04-09 22:06:03 +0000

    cuarta pruebas

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import reactToWebComponent from "react-to-webcomponent";
 import { Home } from "./Home";
 import "./style.css";
+import "./home.css";
+
 
 const WebComponent = reactToWebComponent(Home, React, ReactDOM);
 customElements.define("ruggeri-fit", WebComponent);
